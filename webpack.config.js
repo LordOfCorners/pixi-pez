8e50f4d98d14bfa408b51e8cbcadae17e615ccc6
Michael Kahane <michaeljkahane@gmail.com>
2017-09-26 18:19:52 -0400

    attempt at fixing github pages issue

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
       filename: 'pez.js',
-      path: path.resolve(__dirname, 'dist')
+      path: path.resolve(__dirname)
     },
     plugins: [
         new HtmlWebpackPlugin({
